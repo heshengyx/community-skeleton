36a8277c5b62c8f917779d78fef902a2a10350bb
hes <heshengyx@qq.com>
2015-05-05 11:11:03 +0800

    12

--- src/main/java/com/myself/community/param/RegionQueryParam.java
+++ src/main/java/com/myself/community/param/RegionQueryParam.java
@@ -1,8 +1,28 @@
 package com.myself.community.param;
 
+import java.util.Date;
+
 public class RegionQueryParam extends QueryParam {
 
 	private String name;
+	private String status;
+	private Date createTime;
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
 
 	public String getName() {
 		return name;
